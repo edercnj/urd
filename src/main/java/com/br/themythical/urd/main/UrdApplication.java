59ad2bb94ae2d177b25261f0aa6cfcb3c466d291
Eder Nunes Junior <edercnj@gmail.com>
2019-04-09 22:42:53 -0300

    Create project structure

---
+++ src/main/java/com/br/themythical/urd/main/UrdApplication.java
@@ -0,0 +1,13 @@
+package com.br.themythical.urd.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrdApplication.class, args);
+	}
+
+}
