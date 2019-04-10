96d6397751489d5b5bb8b5feb4dcdd07cee5d5a3
Eder Nunes Junior <edercnj@gmail.com>
2019-04-09 23:29:26 -0300

    Add LogMessage class

--- src/main/java/com/br/themythical/urd/domain/entities/LogMessage.java
+++ src/main/java/com/br/themythical/urd/domain/entities/LogMessage.java
@@ -1,17 +1,14 @@
 package com.br.themythical.urd.domain.entities;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.google.gson.Gson;
 
-import java.net.InetAddress;
 import java.net.URL;
-import java.net.UnknownHostException;
 import java.util.Date;
 import java.util.Objects;
 
-public final class SystemErrorMessage {
+public final class LogMessage {
 
     private String serverName;
-    @Value("$spring.application.name")
     private String serviceName;
     private Date dateTime;
     private int errorCord;
@@ -19,59 +16,64 @@ public final class SystemErrorMessage {
     private String userMessage;
     private URL moreInfo;
 
-    public SystemErrorMessage(int errorCord, String developerMessage, String userMessage, URL moreInfo) {
-        setServerName();
-        setDateTime();
-        this.errorCord = errorCord;
-        this.developerMessage = developerMessage;
-        this.userMessage = userMessage;
-        this.moreInfo = moreInfo;
+    public LogMessage(String serverName, String serviceName, Date dateTime, int errorCord, String developerMessage, String userMessage, URL moreInfo) {
+        setServerName(serverName);
+        setServiceName(serviceName);
+        setDateTime(dateTime);
+        setErrorCord(errorCord);
+        setDeveloperMessage(developerMessage);
+        setUserMessage(userMessage);
+        setMoreInfo(moreInfo);
     }
 
-    private void setServerName() {
-        try {
-            this.serverName = InetAddress.getLocalHost().getHostName();
-        } catch (UnknownHostException e) {
-            this.serverName = "Unknown";
-        }
+    public LogMessage(String serverName, String serviceName, Date dateTime, int errorCord, String developerMessage, String userMessage) {
+        setServerName(serverName);
+        setServiceName(serviceName);
+        setDateTime(dateTime);
+        setErrorCord(errorCord);
+        setDeveloperMessage(developerMessage);
+        setUserMessage(userMessage);
     }
 
-    private void setDateTime() { this.dateTime = new Date(); }
+    public String getServerName() { return serverName; }
+
+    public void setServerName(String serverName) { this.serverName = serverName; }
+
+    public String getServiceName() { return serviceName; }
+
+    public void setServiceName(String serviceName) { this.serviceName = serviceName; }
 
     public Date getDateTime() { return dateTime; }
 
-    public String getServerName() {
-        return serverName;
-    }
+    public void setDateTime(Date dateTime) { this.dateTime = dateTime; }
 
-    public String getServiceName() {
-        return serviceName;
-    }
+    public int getErrorCord() { return errorCord; }
 
-    public int getErrorCord() {
-        return errorCord;
-    }
+    public void setErrorCord(int errorCord) { this.errorCord = errorCord; }
 
-    public String getDeveloperMessage() {
-        return developerMessage;
-    }
+    public String getDeveloperMessage() { return developerMessage; }
 
-    public String getUserMessage() {
-        return userMessage;
-    }
+    public void setDeveloperMessage(String developerMessage) { this.developerMessage = developerMessage; }
 
-    public URL getMoreInfo() {
-        return moreInfo;
-    }
+    public String getUserMessage() { return userMessage; }
+
+    public void setUserMessage(String userMessage) { this.userMessage = userMessage; }
+
+    public URL getMoreInfo() { return moreInfo; }
+
+    public void setMoreInfo(URL moreInfo) { this.moreInfo = moreInfo; }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(getServerName(), getServiceName(), getDateTime(), getErrorCord());
-    }
+    public int hashCode() { return Objects.hash(getServerName(), getServiceName(), getDateTime(), getErrorCord()); }
 
     @Override
     public String toString() {
         return String.format(hashCode() + " | serverName:%s | serviceName:%s1 | dateTime:%s2 | errorCord:%s3 | developerMessage:%s4 | userMessage:%s5 | moreInfo:%s6",
                 getServerName(), getServiceName(), getDateTime(), getErrorCord(), getDeveloperMessage(), getUserMessage(), getMoreInfo());
+    }
+
+    public String toJson() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
     }
 }
