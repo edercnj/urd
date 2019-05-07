bcadeccf8e230ec1a43d6556621d37ad1867cc49
Eder Celeste Nunes Junior <edercnj@gmail.com>
2019-05-07 19:29:50 -0300

    configure profile test to use h2 in unitTest

--- src/test/java/com/br/kerberus/urd/service/test/ServerServiceTest.java
+++ src/test/java/com/br/kerberus/urd/service/test/ServerServiceTest.java
@@ -8,12 +8,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class ServerServiceTest {
 
 	@Autowired
--- src/test/resources/db/migration/V1_0__Init.sql
+++ src/test/resources/db/migration/V1_0__Init.sql
@@ -1,10 +1,10 @@
 ----------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS server(
- 
+
     id bigint PRIMARY KEY,
     hostname varchar(75) UNIQUE NOT NULL,
     creation_date TIMESTAMP NOT NULL
-    
+
 );
 
 CREATE SEQUENCE IF NOT EXISTS server_id_seq start 1 increment 1;
@@ -13,14 +13,14 @@ CREATE INDEX IF NOT EXISTS idx_server_hostname ON server(hostname);
 
 ----------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS application (
- 
+
     id bigint PRIMARY KEY,
     name varchar(75) UNIQUE NOT NULL,
     log_path varchar(250),
     server_id bigint NOT NULL,
     creation_date TIMESTAMP NOT NULL,
-    
-    CONSTRAINT FK_aplication_server_id FOREIGN KEY (server_id) REFERENCES server (id) ON UPDATE NO ACTION ON DELETE NO ACTION
+
+    CONSTRAINT FK_application_server_id FOREIGN KEY (server_id) REFERENCES server (id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 CREATE SEQUENCE IF NOT EXISTS application_id_seq start 1 increment 1;
