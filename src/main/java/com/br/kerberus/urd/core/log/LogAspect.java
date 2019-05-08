2488c43f55eff02310ebe70c0e0fb72b0d0c3546
Eder Celeste Nunes Junior <edercnj@gmail.com>
2019-05-08 18:51:20 -0300

    add aspctJ to exceptions

--- src/main/java/com/br/kerberus/urd/core/log/LogAspect.java
+++ src/main/java/com/br/kerberus/urd/core/log/LogAspect.java
@@ -1,9 +1,10 @@
 package com.br.kerberus.urd.core.log;
 
+import com.br.kerberus.urd.exception.UrdException;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.flywaydb.core.internal.logging.console.ConsoleLog.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -20,15 +21,24 @@ public class LogAspect {
 	public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
 		
 		long start = System.currentTimeMillis();
-
 		Object proceed = joinPoint.proceed();
-
 		long executionTime = System.currentTimeMillis() - start;
-
-		 //System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
 		log.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+
 		return proceed;
 	}
+
+	@AfterThrowing(pointcut = "execution(* com.br.kerberus.urd.*.*(..))", throwing = "ex")
+	public void logUrdException(UrdException ex){
+
+		log.error(ex.getMessage());
+	}
+
+	@AfterThrowing(pointcut = "execution(* com.br.kerberus.urd.*.*(..))", throwing = "ex")
+	public void logException(Exception ex){
+		log.error(ex.getMessage());
+	}
+
 
 	public Logger getLog() { return log; }
 
