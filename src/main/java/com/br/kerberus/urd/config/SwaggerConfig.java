aa993bba009fc5f4b3d890e41005001760a6c93f
Eder Celeste Nunes Junior <edercnj@gmail.com>
2019-05-31 09:06:12 -0300

    configure swagger for only execute in dev

--- src/main/java/com/br/kerberus/urd/config/SwaggerConfig.java
+++ src/main/java/com/br/kerberus/urd/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.br.kerberus.urd.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
@@ -16,6 +17,7 @@ import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
+@Profile("dev")
 public class SwaggerConfig  extends WebMvcConfigurationSupport{                                    
 	@Bean
 	public Docket api() {                
