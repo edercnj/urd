28e2bea451bff36acdfd7a439deed4caa288c557
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2019-10-25 10:36:23 -0300

    add UrdConfig with configuration for spring OAP

--- src/main/java/com/br/kerberus/urd/config/UrdConfig.java
+++ src/main/java/com/br/kerberus/urd/config/UrdConfig.java
@@ -1,30 +1,4 @@
 package com.br.kerberus.urd.config;
 
-import com.br.kerberus.urd.entity.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-@Configuration
 public class UrdConfig {
-
-    public static Server server;
-
-    @Bean
-    protected void setProperties()
-    {
-        try {
-
-            server.setHostname(InetAddress.getLocalHost().getHostName().toUpperCase());
-            server.setIp(InetAddress.getLocalHost().getHostAddress());
-
-        } catch (UnknownHostException e) {
-
-            server.setHostname("unknown");
-            server.setIp("127.0.0.1");
-
-        }
-    }
 }
---
+++ src/test/java/com/br/kerberus/urd/service/test/ExceptionLogServiceTest.java
@@ -0,0 +1,30 @@
+package com.br.kerberus.urd.service.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExceptionLogServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getLog() {
+    }
+
+    @Test
+    public void setLog() {
+    }
+
+    @Test
+    public void logException() {
+    }
+}
